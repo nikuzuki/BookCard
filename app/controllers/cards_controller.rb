641027393d1773375e9004f8ae3a946c02e50151
Kohei Makita <nikuzuki29@gmail.com>
2017-06-23 00:25:38 +0900

    add index
    インデックスページのコントローラとviewを書いた

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,16 @@
+class CardsController < ApplicationController
+  #layout 'cards'
+  def index
+    @cards = Card.all
+  end
+
+  def show
+    
+  end
+
+  def add
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'cards/index'
+  get 'cards', to: 'cards#index'
+  
+  get 'cards/show'
+
+  get 'cards/add'
+
+  get 'cards/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
